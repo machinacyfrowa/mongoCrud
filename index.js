396bae0b1ff6c9ab82ff2ae5373d8355d227ced9
Paweł Zawadzki <admin@machinacyfrowa.pl>
2023-11-06 13:31:24 +0100

    showing all data as table

--- index.js
+++ index.js
@@ -15,9 +15,15 @@ app.get('/', async (req, res) => {
 app.get('/listAll', async (req, res) => {
     res.write("<h1>Lista wszystkich rekordów w bazie</h1>");
     const client = await db.connect();
+    res.write("<table>");
     let list = await db.getAllListings(client);
-    list = list.toString();
-    res.write(list);
+    list.forEach(element => {
+        res.write("<tr>");
+        res.write("<td>" + element.listing_url + "</td>");
+        res.write("<td>" + element.name + "</td>");
+        res.write("</tr>");
+    });
+    res.write("</table>");
     db.close(client);
     res.end();
 })
